73ba6bba30d86cbae1779d5e0bb0595de9fcdec8
Laan33 <103543748+Laan33@users.noreply.github.com>
2023-03-29 15:36:19 +0100

    analog 24

---
+++ twenty4trial2/public/index.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    d = new Date(); //object of date()
+    hr = d.getHours();    
+    min = d.getMinutes();
+    sec = d.getSeconds();
+    
+    
+    hr_rotation = 15 * hr + min / 4 ; //converting current time
+    
+    
+    min_rotation = 6 * min;
+    sec_rotation = 6 * sec;
+  
+    hour.style.transform = `rotate(${hr_rotation}deg)`;
+    minute.style.transform = `rotate(${min_rotation}deg)`;
+    second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
